61b88a80b6c3bad4f4ae6941736d9de590db4f07
Matthew Lim <lim.ma@northeastern.edu>
2024-09-26 11:23:52 -0400

    model for stg generated reviews

---
+++ models/staging/staging_sql/stg_generated_reviews.sql
@@ -0,0 +1,7 @@
+SELECT
+       id                                                                                  AS review_id
+,      listing_id                                                                          AS listing_id
+,      review_score                                                                        AS review_score
+,      review_date                                                                         AS review_date
+FROM
+       {{ source('rental_app', 'generated_reviews') }}
