aa0cd7fcbb311ebc0324fed79edba01d1975d92b
Matthew Lim <lim.k.matthew@gmail.com>
2024-09-27 08:38:23 -0400

    remove non renamed aliases

--- models/staging/staging_sql/stg_calendar.sql
+++ models/staging/staging_sql/stg_calendar.sql
@@ -2,10 +2,10 @@ SELECT
        listing_id                                                                          AS listing_id
 ,      date                                                                                AS availability_date
 ,      available = 't'                                                                     AS is_available
-,      reservation_id                                                                      AS reservation_id
+,      reservation_id
 ,      price                                                                               AS price_dollars
-,      minimum_nights                                                                      AS minimum_nights
-,      maximum_nights                                                                      AS maximum_nights
-,      HASH(listing_id, date)                                                               AS listing_date_key
+,      minimum_nights
+,      maximum_nights
+,      HASH(listing_id, date)                                                              AS listing_date_key
 FROM
        {{ source('rental_app', 'calendar') }}
