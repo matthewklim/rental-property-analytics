49701e02177a88527a85ecada9e297b324a3d377
Matthew Lim <lim.k.matthew@gmail.com>
2024-09-26 23:41:53 -0400

    amenity list to staging model

--- models/staging/staging_sql/stg_amenities_changelog.sql
+++ models/staging/staging_sql/stg_amenities_changelog.sql
@@ -1,6 +1,7 @@
 SELECT
        listing_id                                                                          AS listing_id
 ,      change_at                                                                           AS change_at
+,      PARSE_JSON(amenities)                                                               AS amenities_list
 ,      HASH(listing_id, change_at)                                                         AS amenities_change_key
 FROM
        {{ source('rental_app', 'amenities_changelog') }}
