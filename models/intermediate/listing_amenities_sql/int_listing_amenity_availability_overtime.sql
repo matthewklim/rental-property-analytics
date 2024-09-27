674e6f185be865dcc616b0d46e0f0fbb387b0131
Matthew Lim <lim.k.matthew@gmail.com>
2024-09-27 09:31:09 -0400

    valid to

--- models/intermediate/listing_amenities_sql/int_listing_amenity_availability_overtime.sql
+++ models/intermediate/listing_amenities_sql/int_listing_amenity_availability_overtime.sql
@@ -1,6 +1,7 @@
 SELECT
        listing_id
-,      change_at::DATE                                                                     AS change_date
+,      change_at::DATE                                                                     AS valid_from_date
+,      LEAD(valid_from_date) OVER (PARTITION BY listing_id ORDER BY change_at)             AS valid_to_date
     {{ expand_amenities_to_boolean_columns('stg_amenities_changelog', 'amenities_list') }}
 FROM
     {{ ref('stg_amenities_changelog') }}
