82b3ee17c79d0c913d360bd986ca8d59fca85906
Matthew Lim <lim.k.matthew@gmail.com>
2024-09-27 09:31:24 -0400

    longest stay duration and rank

--- models/mart/listings_sql/listings_daily_summary.sql
+++ models/mart/listings_sql/listings_daily_summary.sql
@@ -8,9 +8,11 @@ SELECT
 ,      stg_calendar.maximum_nights
 ,      stg_calendar.listing_date_key
 ,      stg_listings.neighborhood_name
+,      int_consecutive_stays.longest_stay_duration
 ,      int_listing_amenity_availability_overtime."air conditioning"                        AS has_air_conditioning
 ,      int_listing_amenity_availability_overtime."lockbox"                                 AS has_lockbox
 ,      int_listing_amenity_availability_overtime."first aid kit"                           AS has_first_aid_kit
+,      DENSE_RANK() OVER (ORDER BY longest_stay_duration DESC NULLS LAST)                  AS longest_stay_duration_rank
 FROM
        {{ ref('stg_calendar') }}
 JOIN
@@ -18,8 +20,12 @@ JOIN
 ON
        stg_calendar.listing_id = stg_listings.listing_id
 LEFT JOIN
+       {{ ref('int_consecutive_stays') }}
+ON
+       stg_calendar.listing_id = int_consecutive_stays.listing_id
+LEFT JOIN
        {{ ref('int_listing_amenity_availability_overtime') }}
 ON
        stg_calendar.listing_id = int_listing_amenity_availability_overtime.listing_id
 AND
-       stg_calendar.availability_date = int_listing_amenity_availability_overtime.change_date
+       stg_calendar.availability_date BETWEEN int_listing_amenity_availability_overtime.valid_from_date AND COALESCE(int_listing_amenity_availability_overtime.valid_to_date, CURRENT_TIMESTAMP())
